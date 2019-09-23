857667a04395b0567d493f0f5d348c8a2f08932b
blukasho <bodik1w@gmail.com>
2019-09-23 18:07:20 +0300

    add links.c

---
+++ srcs/links.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   links.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: blukasho <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/23 14:20:18 by blukasho          #+#    #+#             */
+/*   Updated: 2019/09/23 18:06:45 by blukasho         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <lemin.h>
+
+static int	links_prepare_to_read(t_lemin *lemin)
+{
+	int		size_one_side;
+	int		counter;
+	char	**graph_map;
+	t_rooms	*rooms;
+
+	size_one_side = get_len_rooms(lemin->rooms);
+	graph_map = (char **)malloc(++size_one_side * sizeof(char *));
+	counter = size_one_side - 1;
+	graph_map[counter] = NULL;
+	while (--counter >= 0)
+	{
+		graph_map[counter] = ft_memset((char *)malloc(size_one_side *
+		sizeof(char)), FILLCH, size_one_side);
+		graph_map[counter][size_one_side - 1] = 0;
+	}
+	lemin->map = graph_map;
+	counter = 0;
+	rooms = lemin->rooms;
+	while (rooms && (rooms = rooms->next))
+		rooms->pos = ++counter;
+	return (0);
+}
+
+int			get_links(char *input, t_lemin *lemin)
+{
+	links_prepare_to_read(lemin);
+	return (0);
+}
