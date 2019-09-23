8544489d855ce24ee9860b9cd193d6e0b61130af
blukasho <bodik1w@gmail.com>
2019-09-23 08:30:20 +0300

    fix norme rooms.c, add new function because get_rooms() has over 25 lines

--- srcs/rooms.c
+++ srcs/rooms.c
@@ -6,26 +6,25 @@
 /*   By: blukasho <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/05 15:22:32 by blukasho          #+#    #+#             */
-/*   Updated: 2019/09/19 14:04:38 by blukasho         ###   ########.fr       */
+/*   Updated: 2019/09/23 08:28:08 by blukasho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <lemin.h>
 
-void		valid_room_coords(t_rooms *room, char *coords)
+void			valid_room_coords(t_rooms *room, char *coords)
 {
 	if (!coords)
 		SETANDPERROR(5, "ERROR. Broken coords.");
-	if (!errno && (!(coords = ft_strchr(coords, ' ')) || (!ISDIGIT(*(++coords)) &&
-		*coords != '-' && *coords != '+')))
+	if (!errno && (!(coords = ft_strchr(coords, ' ')) || (!ISDIGIT(*(++coords))
+		&& *coords != '-' && *coords != '+')))
 		SETANDPERROR(5, "ERROR. Broken coord_x.");
 	else if ((*coords == '-' || *coords == '+'))
 		SETANDPERROR(5, "ERROR. Forbidden sign '+' or '-'");
 	else if ((room->coord_x = ft_atoi(coords)) < 0)
 		SETANDPERROR(5, "ERROR. Coords must be more or equal 0.");
-	if (!errno)
-		while (ISDIGIT(*coords))
-			++coords;
+	while (!errno && ISDIGIT(*coords))
+		++coords;
 	if (!errno && (!*coords || *(coords++) != ' '))
 		SETANDPERROR(5, "ERROR. Broken coord_y.");
 	else if (!errno && !ISDIGIT(*coords) && *coords != '-' && *coords != '+')
@@ -89,6 +88,15 @@ static void		add_room(t_lemin *lemin, char *input, int type)
 		valid_room_coords(tmp, input);
 }
 
+static int		check_errors_get_rooms(t_lemin *lemin)
+{
+	if (!lemin->start_room)
+		SETANDPERROR(5, "ERROR. No \"##start\" command.");
+	else if (!lemin->end_room)
+		SETANDPERROR(5, "ERROR. No \"##end\" command.");
+	return (0);
+}
+
 char			*get_rooms(t_lemin *lemin)
 {
 	char		*input;
@@ -98,20 +106,16 @@ char			*get_rooms(t_lemin *lemin)
 		if (!errno && (ISCOMMENT(input) || (ISCOMMAND(input) && !ISSTART(input)
 			&& !ISEND(input))))
 			ft_strdel(&input);
-		else if (!errno && ISCOMMAND(input) && ISSTART(input) && !ft_strdel(&input))
+		else if (!errno && ISCOMMAND(input) && ISSTART(input) &&
+				!ft_strdel(&input))
 			add_room(lemin, NULL, STARTROOM);
-		else if (!errno && ISCOMMAND(input) && ISEND(input) && !ft_strdel(&input))
+		else if (!errno && ISCOMMAND(input) && ISEND(input) &&
+				!ft_strdel(&input))
 			add_room(lemin, NULL, ENDROOM);
 		else if (!errno && !ISLINK(input))
 			add_room(lemin, input, DEFAULTROOM);
-		else if (!errno)
-		{
-			if (!lemin->start_room)
-				SETANDPERROR(5, "ERROR. No \"##start\" command.");
-			else if (!lemin->end_room)
-				SETANDPERROR(5, "ERROR. No \"##end\" command.");
+		else if (!errno && !check_errors_get_rooms(lemin))
 			return (input);
-		}
 		if (input)
 			ft_strdel(&input);
 	}
